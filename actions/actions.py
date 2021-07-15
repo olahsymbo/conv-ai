88d0170313aad7eebb9c4b5dec11b879d9193209
Olasimbo <olahsymbo@gmail.com>
2021-07-15 08:58:31 +0100

    add webchat

--- actions/actions.py
+++ actions/actions.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
-from rasa_sdk.events import AllSlotsReset
+from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
 import psycopg2
@@ -51,6 +51,7 @@ class ActionPurchaseProducts(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         # import ipdb; ipdb.set_trace()
+        print(tracker.sender_id)
         current_product = tracker.get_slot('product')
         product_price = tracker.get_slot('price')
         criteria = tracker.get_slot('criteria')
@@ -59,13 +60,13 @@ class ActionPurchaseProducts(Action):
         # result = cursor.query(q)
         # print(result)
         if not criteria:
-            dispatcher.utter_message(text="here are {}".format(current_product))
-            dispatcher.utter_message(text="www.amazon.com")
+            dispatcher.utter_message(text="here is {}".format(current_product))
+            dispatcher.utter_message(text="https://www.amazon.com/{}/1".format(current_product))
         elif not product_price:
-            dispatcher.utter_message(text="here are {} of size {}".format(current_product, criteria))
+            dispatcher.utter_message(text="here is {} of size {}".format(current_product, criteria))
         elif product_price:
-            dispatcher.utter_message(text="here are {} {}".format(product_price, current_product))
-        return [AllSlotsReset()]
+            dispatcher.utter_message(text="here is {} {}".format(product_price, current_product))
+        return []
 
 
 class ActionAddCart(Action):
@@ -84,5 +85,45 @@ class ActionAddCart(Action):
                            (id, current_product, "dunder", 10, "www.amazon.com", datetime.now().strftime("%Y-%m-%d")))
             con.commit()
             dispatcher.utter_message(text="Your {} has been added to cart".format(current_product))
+
+        return [SlotSet('product', None)]
+
+
+class ActionCheckout(Action):
+
+    def name(self) -> Text:
+        return "action_checkout"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        current_product = tracker.get_slot('product')
+        if current_product is not None:
+            cursor.execute("SELECT product FROM shop WHERE amount = '100' ")
+            rows = cursor.fetchall()
+            dispatcher.utter_message(text="You have {} items to pay for. "
+                                          "Click [here](https://www.paystack.com) to proceed".format(len(rows[:4])))
+        elif not current_product:
+            dispatcher.utter_message(text="sorry you can't make payment without any product in cart")
+
+        return []
+
+
+class ShowPurchaseHistory(Action):
+
+    def name(self) -> Text:
+        return "action_purchase_history"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        cursor.execute("SELECT product FROM shop WHERE date = '2021-07-06 00:00:00' ")
+        rows = cursor.fetchall()
+        all_items = [r[0] for r in rows[:3]]
+        if not all_items:
+            dispatcher.utter_message(text="purchase history is empty")
+        elif all_items:
+            dispatcher.utter_message(text="You've purchased {} items in the past 2 weeks".format(len(all_items)))
+            dispatcher.utter_message(text="These are the items {}".format(all_items))
 
         return []
--- kb/views.py
+++ kb/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import logging
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Shop
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+
+class AskConv(generics.GenericAPIView):
+
+    queryset = Shop.objects.all()
+
+    def get_actions(self, request, *args, **kwargs):
+        super(AskConv, self).get_actions(request, args, kwargs)
+
+        instance = self.get_object()
+
+        serializer = self.get_serializer(instance)
+
+        data = serializer.data
+
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully retrieved",
+                    "result": data}
+
+        logger.info(data)
+
+        return Response(response)
